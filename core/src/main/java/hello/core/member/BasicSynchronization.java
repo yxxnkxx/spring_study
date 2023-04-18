03c2896315c857ca6c68d6bfb2067c7c18a8202a
YunGyung Lee <yxxnkxx@gmail.com>
2023-04-18 15:21:43 +0900

    feat: 스프링 핵심 원리 기본편 도메인

---
+++ core/src/main/java/hello/core/member/BasicSynchronization.java
@@ -0,0 +1,39 @@
+package hello.core.member;
+
+public class BasicSynchronization {
+    private String mMessage;
+
+    public static void main(String[] agrs) {
+        BasicSynchronization temp = new BasicSynchronization();
+
+        System.out.println("Test start!");
+        new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                temp.callMe("Thread1");
+            }
+        }).start();
+
+        new Thread(() -> {
+            for (int i = 0; i < 1000; i++) {
+                temp.callMe("Thread2");
+            }
+        }).start();
+        System.out.println("Test end!");
+
+    }
+
+    public synchronized void callMe(String whoCallMe) {
+        mMessage = whoCallMe;
+
+        try {
+            long sleep = (long) (Math.random() * 100);
+            Thread.sleep(sleep);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        if (!mMessage.equals(whoCallMe)) {
+            System.out.println(whoCallMe + " | " + mMessage);
+        }
+    }
+}
