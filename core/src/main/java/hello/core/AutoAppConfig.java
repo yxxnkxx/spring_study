7c44df48041e09b9cd2a4742d48950631164e4eb
YunGyung Lee <yxxnkxx@gmail.com>
2023-04-20 14:44:48 +0900

    feat: 의존관계 자동 주입

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -14,8 +14,4 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
 
-    @Bean(name = "memoryMemberRepository")
-    MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
 }
--- core/src/main/java/hello/core/order/Order.java
+++ core/src/main/java/hello/core/order/Order.java
@@ -1,5 +1,10 @@
 package hello.core.order;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Order {
 
     private Long memberId;
@@ -14,42 +19,6 @@ public class Order {
         this.discountPrice = discountPrice;
     }
 
-    public int calculatePrice() {
-        return itemPrice - discountPrice;
-    }
-
-    public Long getMemberId() {
-        return memberId;
-    }
-
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
-    }
-
-    public String getItemName() {
-        return itemName;
-    }
-
-    public void setItemName(String itemName) {
-        this.itemName = itemName;
-    }
-
-
-    public int getItemPrice() {
-        return itemPrice;
-    }
-
-    public void setItemPrice(int itemPrice) {
-        this.itemPrice = itemPrice;
-    }
-
-    public int getDiscountPrice() {
-        return discountPrice;
-    }
-
-    public void setDiscountPrice(int discountPrice) {
-        this.discountPrice = discountPrice;
-    }
 
     @Override
     public String toString() {
